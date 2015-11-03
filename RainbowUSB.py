dd866243df43e3b35b1877992ed617aa002fa9d2
Rodrigo Groppa <rgroppa@gmail.com>
2015-11-02 21:52:15 -0500

    Updated webserver to flask. Successfully connected and hit TestUSB. Usb link and webserver host are still fragile

--- RainbowUSB.py
+++ RainbowUSB.py
@@ -1,11 +1,10 @@
 import usb.core
 import usb.util
 from ClrMsg import ClrMsg
-from struct import *
+
 
 class Rainbow:
-
-    ep = None
+    endpoint = None
     ON1 = "\x80\x00\x00\x00"
     ON2 = "\xC0\x00\x00\x00"
 
@@ -17,12 +16,11 @@ class Rainbow:
         if dev is None:
             raise ValueError('Device not found')
 
-        reattach = False
         if dev.is_kernel_driver_active(2):
-                dev.detach_kernel_driver(2)
-                print "kernel driver detached"
+            dev.detach_kernel_driver(2)
+            print "Dettached Kernel"
         else:
-            print "Kernel Already Detached"
+            print "Kernel Already Dettached"
 
         # set the active configuration. With no arguments, the first
         # configuration will be the active one
@@ -30,30 +28,36 @@ class Rainbow:
 
         # get an endpoint instance
         cfg = dev.get_active_configuration()
-        intf = cfg[(1,0)]
 
-        ep = usb.util.find_descriptor(
-            intf,
+        interface = cfg[(1, 0)]
+
+        self.endpoint = usb.util.find_descriptor(
+            interface,
             # match the first OUT endpoint
             custom_match=lambda e:
             usb.util.endpoint_direction(e.bEndpointAddress) ==
             usb.util.ENDPOINT_OUT)
 
-        if ep is None:
+        if self.endpoint is None:
             print "Endpoint Not Found"
 
     def TestUSBs(self):
         for x in range(0, 48):
-            msg = ClrMsg(1,x,127,0 ,0)
-            ep.write(msg.payload)
+            msg = ClrMsg(1, x, 127, 0, 0)
+            self.endpoint.write(msg.payload)
 
         for x in range(0, 32):
-            msg = ClrMsg(2,x,0,127,0)
-            ep.write(msg.payload)
+            msg = ClrMsg(2, x, 0, 127, 0)
+            self.endpoint.write(msg.payload)
 
-        ep.write(ON1)
-        ep.write(ON2)
+        self.endpoint.write(self.ON1)
+        self.endpoint.write(self.ON2)
 
+    def setColor(self, strip, pos, red, green, blue):
+        if self.endpoint is None:
+            return "No usb connected"
 
-
+        print "Strip %1 Pos %2 Red %3 Green %4 Blue %5".format(strip, pos, red, green, blue)
+        msg = ClrMsg(strip, pos, red, green, blue)
+        return self.endpoint.write(msg.payload)
 
--- webserver.py
+++ webserver.py
@@ -1,13 +1,27 @@
-import web
+from flask import Flask, request
+import json
+import RainbowUSB
 
-urls = (
-	'/', 'index'
-)
+#Set up dependencies
+app = Flask(__name__)
+rainbow = RainbowUSB.Rainbow()
 
-class index:
-	def GET(self):
-		return "Hello World!"
 
-if __name__ == "__main__":
-	app = web.application(urls, globals())
-	app.run()
+@app.route('/Rainbow', methods=['POST'])
+def handle_rainbow():
+    data = request.get_json(force=True)
+    strip = int(data.get("strip"))
+    pos = int(data.get("position"))
+    red = int(data.get("red"))
+    green = int(data.get("green"))
+    blue = int(data.get("blue"))
+    return rainbow.setColor(strip, pos, red, green, blue)
+
+@app.route('/Test', methods=['POST'])
+def test_usb():
+    return rainbow.TestUSBs()
+
+
+# Connect USB and start webserver
+if __name__ == '__main__':
+    app.run(debug=True)
