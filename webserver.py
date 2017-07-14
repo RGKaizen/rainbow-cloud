3eed63a0608fa34a5b2bc8272d01c261c90deef0
Rodrigo Groppa <rgroppa@gmail.com>
2017-07-14 00:07:59 -0400

    Cleaner

--- webserver.py
+++ webserver.py
@@ -15,7 +15,7 @@ def handle_rainbow():
         data = request.get_json(force=True)
         pixels = data["pixels"]
         for p in pixels:
-            _PixelState[int(p.get("channel"))][int(p.get("pos"))] = (p["red"], p["green"], p["blue"])
+            _PixelState[p["channel"]][p["pos"]] = (p["red"], p["green"], p["blue"])
 
         for c in range(_ChannelCount):            
             if(_Client.put_pixels(_PixelState[c], channel=c)):
@@ -24,16 +24,21 @@ def handle_rainbow():
     except Exception:
         return '\tInvalidInput\n'
 
-@_App.route('/On', methods=['GET'])
-def on():
+@_App.route('/OnBoth', methods=['GET'])
+def onBoth():
     pixels_out = []
-    for c in range(_ChannelCount):
-        for ii in range(_LedCount):
-            red = 256
-            green = 256
-            blue = 256
-            pixels_out.append((red, green, blue))
-        _Client.put_pixels(pixels_out, channel=c)
+    for ii in range(_LedCount):
+        red = 256
+        green = 0
+        blue = 0
+        pixels_out.append((red, green, blue))
+    _Client.put_pixels(pixels_out, channel=0)
+    for ii in range(_LedCount):
+        red = 0
+        green = 256
+        blue = 0
+        pixels_out.append((red, green, blue))
+    _Client.put_pixels(pixels_out, channel=1)
     return 'okay'
 
 @_App.route('/OnA', methods=['GET'])
@@ -44,6 +49,12 @@ def onA():
         green = 0
         blue = 0
         pixels_out.append((red, green, blue))
+    _Client.put_pixels(pixels_out, channel=0)
+    return 'okay'
+
+@_App.route('/OnB', methods=['GET'])
+def onB():
+    pixels_out = []
     _Client.put_pixels(pixels_out, channel=0)
     for ii in range(_LedCount):
         red = 0
