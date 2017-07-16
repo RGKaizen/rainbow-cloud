b55f1171f0331a5b308ccd44386e6a828afbbe8a
Rodrigo Groppa <rgroppa@gmail.com>
2017-07-16 15:50:55 -0400

    Revert

--- webserver.py
+++ webserver.py
@@ -1,87 +1,65 @@
-from flask import Flask, request
-import opc
-
-_App = Flask(__name__)
-_IPPort = '127.0.0.1:7890'
-_Client = opc.Client(_IPPort, verbose=True)
-_LedCount = 60
-_ChannelCount = 2
-
-_PixelState = [[(0,0,0) for x in range(_LedCount)] for y in range(_ChannelCount)]
-
 @_App.route('/Rainbow', methods=['POST'])
 def handle_rainbow():
-    try:
-        data = request.get_json(force=True)
-        pixels = data["pixels"]
-        for p in pixels:
-            _PixelState[p["channel"]][p["pos"]] = (p["red"], p["green"], p["blue"])
-
-        for c in range(_ChannelCount):            
-            if(_Client.put_pixels(_PixelState[0], channel=1)):
-                print('\tsuccess {}\n').format(c)
-        return '\tfail\n'
-    except Exception:
-        return '\tInvalidInput\n'
-
+   try:
+       data = request.get_json(force=True)
+       pixels = data["pixels"]
+       for p in pixels:
+           _PixelState[p["channel"]][p["pos"]] = (p["red"], p["green"], p["blue"])
+ 
+       for c in range(_ChannelCount):            
+           if(_Client.put_pixels(_PixelState[c], channel=c)):
+               print('\tsuccess {}\n').format(c)
+       return '\tfail\n'
+   except Exception:
+       return '\tInvalidInput\n'
+ 
 @_App.route('/OnBoth', methods=['GET'])
 def onBoth():
-    pixels_out = []
-    for ii in range(_LedCount):
-        red = 127
-        green = 127
-        blue = 0
-        pixels_out.append((red, green, blue))
-    #_Client.put_pixels(pixels_out, channel=0)
-    for ii in range(_LedCount):
-        red = 0
-        green = 127
-        blue = 127
-        pixels_out.append((red, green, blue))
-    _Client.put_pixels(pixels_out, channel=1)
-    return 'okay'
-
+   pixels_out = []
+   for ii in range(_LedCount):
+       red = 256
+       green = 0
+       blue = 0
+       pixels_out.append((red, green, blue))
+   _Client.put_pixels(pixels_out, channel=0)
+   for ii in range(_LedCount):
+       red = 0
+       green = 256
+       blue = 0
+       pixels_out.append((red, green, blue))
+   _Client.put_pixels(pixels_out, channel=1)
+   return 'okay'
+ 
 @_App.route('/OnA', methods=['GET'])
 def onA():
-    pixels_out = []
-    for ii in range(_LedCount):
-        red = 256
-        green = 0
-        blue = 0
-        pixels_out.append((red, green, blue))
-    _Client.put_pixels(pixels_out, channel=0)
-    return 'okay'
-
+   pixels_out = []
+   for ii in range(_LedCount):
+       red = 256
+       green = 0
+       blue = 0
+       pixels_out.append((red, green, blue))
+   _Client.put_pixels(pixels_out, channel=0)
+   return 'okay'
+ 
 @_App.route('/OnB', methods=['GET'])
 def onB():
-    pixels_out = []
-    for ii in range(_LedCount):
-        red = 0
-        green = 256
-        blue = 0
-        pixels_out.append((red, green, blue))
-    _Client.put_pixels(pixels_out, channel=1)
-    return 'okay'
-
+   pixels_out = []
+   for ii in range(_LedCount):
+       red = 0
+       green = 256
+       blue = 0
+       pixels_out.append((red, green, blue))
+   _Client.put_pixels(pixels_out, channel=1)
+   return 'okay'
+ 
 @_App.route('/Off', methods=['GET'])
 def off():
-    pixels_out = []
-    for ii in range(_LedCount):
-        red = 0
-        green = 0
-        blue = 0
-        pixels_out.append((red, green, blue))
-    #_Client.put_pixels(pixels_out, channel=0)
-    _Client.put_pixels(pixels_out, channel=1)
-    return 'okay'
-
-# Connect to FC Server and start webserver
-if __name__ == '__main__':
-    _App.run(host='0.0.0.0', debug=True)
-    if _Client.can_connect():
-        print('    connected to FCServer: %s' % _IPPort)
-    else:
-        # can't connect, but keep running in case the server appears later
-        print('    WARNING: could not connect to %s' % _IPPort)
-    print('')
-
+   pixels_out = []
+   for c in range(_ChannelCount):
+       for ii in range(_LedCount):
+           red = 0
+           green = 0
+           blue = 0
+           pixels_out.append((red, green, blue))
+       _Client.put_pixels(pixels_out, channel=c)
+   return 'okay'
